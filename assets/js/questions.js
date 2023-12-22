5cd4fa491577da1bcad1b4eb163a88f07fb9a3f3
howarceus <howarceus@gmail.com>
2023-12-22 19:47:17 +0000

    question object added into question js

---
+++ assets/js/questions.js
@@ -0,0 +1,10 @@
+// questions.js
+
+const questions = [
+    {
+      question: "What is JavaScript?",
+      choices: ["A programming language", "A fruit", "A car brand"],
+      correctAnswer: "A programming language"
+    },
+    // Add more questions...
+  ];
